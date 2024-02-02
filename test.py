fb9d192033dbe46f2d368be90a8328fd9ca1c4ec
aktan <aktifman@gmail.com>
2024-02-02 23:08:22 +0200

    test

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-def convert_to_float_or_str(value):
-    try:
-        result = float(value)
-        return result
-    except:
-        return value
-proverka = convert_to_float_or_str('  1   5 '.strip().replace(" ", ""))
-print(proverka)
+print('hello world')
